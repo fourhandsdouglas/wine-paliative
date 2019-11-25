7f96b027cc984390a3f3d35c843b1a673442a746
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 11:07:46 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -18,7 +18,7 @@
         gtag('config', 'UA-5733415-1', {'optimize_id': 'GTM-NS39JSJ'});
 
         if(url.includes(amp)){
-            document.body.innerHTML = 'Aguarde...';
+            document.body.innerHTML = '<div><img src="https://img.wine.com.br/fenix/image/loading.svg "><h2>Aguarde...</h2></div><style>body{height:100vh;display:flex;align-items:center;justify-content:center;}</style>';
             gtag('send', 'event', 'Erro', 'Manutencao', fileName);
             setTimeout(() => {
                 console.log("Redirect");
