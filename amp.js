642f5086b04d7eabb2de9e9adcb04b1afbfaf18e
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 08:50:04 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
     let amp = "&amp;";
     let url = document.URL;
-    url.includes(amp) ? window.location.replace(url.replace(amp, "&")) : null;
-}, 200);
+    url.includes(amp) ? window.location.replace(url.replace(new RegExp(amp, 'g'), "&")) : null;
+}, 1);
