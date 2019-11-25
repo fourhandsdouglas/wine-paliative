19bb30134102a1cb3dbac62bc55ad8e6440c228a
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 09:46:47 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -6,16 +6,16 @@
 
     dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=UA-5733415-1");
 
+    var url = window.location.pathname;
+    var fileName = url.substring(url.lastIndexOf('/')+1);
+    var amp = "&amp;";
+    var url = document.URL;
+
     setTimeout(() => {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'UA-5733415-1', {'optimize_id': 'GTM-NS39JSJ'});
-
-        var url = window.location.pathname;
-        var fileName = url.substring(url.lastIndexOf('/')+1);
-        var amp = "&amp;";
-        var url = document.URL;
 
         if(url.includes(amp)){
             document.body.innerHTML = 'Aguarde...';
@@ -24,5 +24,7 @@
                 console.log("Redirect");
                 //window.location.replace(url.replace(new RegExp(amp, 'g'), "&"));
             }, 1000);
+        }else{
+            console.log("NOT Redirect");
         }
     }, 1000);
