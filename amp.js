40745ccfe3ca6fa0ea5ce77523f1ac40a4bc4e36
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 12:10:16 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -1,34 +1,31 @@
-    document.body.style.opacity = "0";
-    function dynamicallyLoadScript(url) {
-        var script = document.createElement("script");
-        script.src = url;
-        document.head.appendChild(script);
-    }
+    document.body.style.opacity = "01";
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){
+        console.log("gtag");
+        dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-5733415-1', {'optimize_id': 'GTM-NS39JSJ'});
 
-    dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=UA-5733415-1");
-
-    var pathname = window.location.pathname;
-    var fileName = pathname.substring(pathname.lastIndexOf('/')+1);
+    var url = window.location.pathname;
+    var fileName = url.substring(url.lastIndexOf('/')+1);
     var amp = "&amp;";
     var url = document.URL;
 
-    setTimeout(() => {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-5733415-1', {'optimize_id': 'GTM-NS39JSJ'});
+    if(url.includes(amp)){
+        document.body.innerHTML = '<div><img src="https://img.wine.com.br/fenix/image/loading.svg "><h2>Aguarde...</h2></div><style>body{height:100vh;display:flex;align-items:center;justify-content:center;}</style>';
+        
+        setTimeout(() => {
+            window.location.replace(url.replace(new RegExp(amp, 'g'), "&")+"#redir");
+        }, 3000);
 
-        if(url.includes(amp)){
-            console.log("Rdirect After Send Event");
-            document.body.innerHTML = '<div><img src="https://img.wine.com.br/fenix/image/loading.svg "><h2>Aguarde...</h2></div><style>body{height:100vh;display:flex;align-items:center;justify-content:center;}</style>';
-            gtag('send', 'event', 'Manuteção', fileName, document.referrer, {
-                'event_callback' : function(){
-                    console.log("Call");
-                    window.location.replace(url.replace(new RegExp(amp, 'g'), "&"));
-                }
-            });
-        }else{
-            console.log("NOT Redirect");
-        }
-        document.body.style.opacity = "1";
-    }, 1000);
+        console.log('send', 'event', 'Manuteção', fileName, document.referrer);
+            
+        gtag('send', 'event', 'Manuteção', fileName, document.referrer, {
+            // 'transport': 'beacon',
+            'hitCallback' : function(){
+                console.log("Call");
+                window.location.replace(url.replace(new RegExp(amp, 'g'), "&")+"#redirGag");
+            }
+        });
+    }
+    document.body.style.opacity = "1";
