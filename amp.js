89f30284646dee257ca331d0dc2d066520a424d7
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 09:38:08 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -1,5 +1,27 @@
-setTimeout(() => {
-    let amp = "&amp;";
-    let url = document.URL;
-    url.includes(amp) ? window.location.replace(url.replace(new RegExp(amp, 'g'), "&")) : null;
-}, 1);
+    function dynamicallyLoadScript(url) {
+        var script = document.createElement("script");
+        script.src = url;
+        document.head.appendChild(script);
+    }
+
+    function dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=UA-5733415-1");
+
+    setTimeout(() => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-5733415-1', {'optimize_id': 'GTM-NS39JSJ'});
+
+        var url = window.location.pathname;
+        var fileName = url.substring(url.lastIndexOf('/')+1);
+        var amp = "&amp;";
+        var url = document.URL;
+
+        if(url.includes(amp)){
+            document.body.innerHTML = 'Aguarde...';
+            ga('send', 'event', 'Erro', 'Manutencao', fileName);
+            setTimeout(() => {
+                window.location.replace(url.replace(new RegExp(amp, 'g'), "&"));
+            }, 1000);
+        }
+    }, 1000);
