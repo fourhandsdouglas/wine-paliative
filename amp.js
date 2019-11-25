5afb731f95949e6891673e45c992905b2b2cb42c
Douglas Gonçalves <56445259+fourhandsdouglas@users.noreply.github.com>
2019-11-25 11:43:23 -0300

    Update amp.js

--- amp.js
+++ amp.js
@@ -20,10 +20,12 @@
 
         if(url.includes(amp)){
             document.body.innerHTML = '<div><img src="https://img.wine.com.br/fenix/image/loading.svg "><h2>Aguarde...</h2></div><style>body{height:100vh;display:flex;align-items:center;justify-content:center;}</style>';
-            gtag('send', 'event', 'Manuteção', fileName, document.referrer, {' event_callback' : function (){
-                alert("Call");
-                window.location.replace(url.replace(new RegExp(amp, 'g'), "&"));
-            }});
+            gtag('send', 'event', 'Manuteção', fileName, document.referrer, {
+                'event_callback' : function(){
+                    alert("Call");
+                    window.location.replace(url.replace(new RegExp(amp, 'g'), "&"));
+                }
+            });
         }else{
             console.log("NOT Redirect");
         }
